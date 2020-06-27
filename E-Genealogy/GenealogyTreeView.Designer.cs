f60f3a0f58ac8a170b3a8f281e9d832e04f514f9
Zhang GengYuan <873918442@qq.com>
2020-06-27 15:00:54 +0800

    关系树状图模块更新！

---
+++ E-Genealogy/GenealogyTreeView.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace E_Genealogy
+{
+    partial class GenealogyTreeView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(5, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1200, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint_1);
+            // 
+            // GenealogyTreeView
+            // 
+            this.ClientSize = new System.Drawing.Size(1210, 608);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "GenealogyTreeView";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
